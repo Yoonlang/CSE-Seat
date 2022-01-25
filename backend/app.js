68da55bd17c24627421daf3bf61a3e805b0dbc67
chanwooDev <pove2019@gmail.com>
2022-01-25 23:47:24 +0900

    [feat] back-end start with nodejs/express

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+/*
+    CSE-Seat project
+    
+    18 배찬우
+    18 최윤석
+
+*/
+const express = require('express');
+const mysql = require('mysql');
+const app = express()
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require ('passport-local').Strategy
+const flash = require('connect-flash')
+const router = require('./api/router/index')
+const fs = require('fs');
+var jsonFile = fs.readFileSync('./config/keys.json', 'utf8');
+const DB_key = JSON.parse(jsonFile).DB_key;
+const sessionKeySecret = JSON.parse(jsonFile).session_key.secret;
+
+//if you want to access my DB, contant to chanwooDev / pove2019@gmail.com 
+let connection = mysql.createConnection(DB_key)
+connection.connect();
+
+app.listen(3000, function(){
+    console.log("start!! express server on port 3000")
+});
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+
+app.use(session({
+    secret: sessionKeySecret,
+    resave: false,
+    saveUninitialized: true 
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(router)
+
