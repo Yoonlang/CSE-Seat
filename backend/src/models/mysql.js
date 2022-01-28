81b58096131ed777c5f01229642c1ab22fc593f1
chanwooDev <pove2019@gmail.com>
2022-01-28 21:38:58 +0900

    [feat] Create a database pool loader using the single tone pattern

--- backend/src/models/mysql.js
+++ backend/src/models/mysql.js
@@ -1,18 +1,34 @@
 //if you want to access my DB, contant to chanwooDev / pove2019@gmail.com 
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const path = require('path')
 const DB_key = require(__dirname+'/../config').DB_key;
-const pool = mysql.createPool(DB_key);
-module.exports = function getConnection(callback){
-    pool.getConnection(function(err, conn){  
-        if ( err ) 
-            throw err;
-        else{
-             callback(conn); 
-             conn.release();
-        }
-    });
+
+pool = mysql.createPool(DB_key);
+
+pool.getConnection(function(err, conn){  
+    if ( err ) 
+        throw err;
+    else{
+        console.log('Database connected successfully');
+        conn.release();
+    }
+});
+module.exports = {
+    getPool : ()=>{return pool},
+
+    getConnection : async (callback) => {
+        pool.getConnection(function (err, conn) {
+            if(!err) {
+              callback(conn);
+            }
+            else{
+                throw err;
+            }
+        });
+    },
 }
+
+
 
 
 
