d6948719724610129c8f0ebcde202f3624ed3466
chanwooDev <pove2019@gmail.com>
2022-01-31 17:43:07 +0900

    [feat] Remove await from index

--- backend/src/loaders/index.js
+++ backend/src/loaders/index.js
@@ -2,7 +2,7 @@ expressLoader = require('./express')
 sessionLoader = require('./session')
 
 module.exports = async ({expressApp}) => {
-  await expressLoader({app:expressApp});
+  expressLoader({app:expressApp});
   console.log('Express Initialized');
   sessionLoader({app:expressApp});
   console.log('Session Initialized');
