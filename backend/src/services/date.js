ae07b384ba1e653633851b41e31f2f66630dcd8a
chanwooDev <pove2019@gmail.com>
2022-02-06 01:17:15 +0900

    Feat: Add functions to find the date today and tomorrow

---
+++ backend/src/services/date.js
@@ -0,0 +1,28 @@
+
+const getTodayDate = () => {
+    today = new Date();
+    if (today.getHours() < 6){
+        today.setDate(today.getDate() -1);
+    }
+    year = today.getFullYear();
+    month = ('0'+(today.getMonth()+1)).slice(-2);
+    day = ('0'+today.getDate()).slice(-2);
+    return year + '-' + month + '-' + day;
+}
+
+const getTomorrowDate = () => {
+    today = new Date();
+    today.setDate(today.getDate()+1)
+    if (today.getHours() < 6){
+        today.setDate(today.getDate() -1);
+    }
+    year = today.getFullYear();
+    month = ('0'+(today.getMonth()+1)).slice(-2);
+    day = ('0'+(today.getDate())).slice(-2);
+    return year + '-' + month + '-' + day;
+}
+
+module.exports = {
+    getTodayDate : getTodayDate(),
+    getTomorrowDate : getTomorrowDate()
+}
