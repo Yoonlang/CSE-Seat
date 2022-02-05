3e4e8d81df0e7287e5c28d579ab135a2d1ac3408
yoonlang <cdt9473@gmail.com>
2022-02-05 23:37:28 +0900

    [feat] Update meta in head

--- frontend/components/others/headTitle.jsx
+++ frontend/components/others/headTitle.jsx
@@ -4,6 +4,7 @@ import React from "react";
 const HeadTitle = ({title}) => {
     return (
         <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>{title} | CSE Seat</title>
         </Head>
     );
