4b76612357b3fac7902e4ba4a000cb6223f0c0c3
chanwooDev <pove2019@gmail.com>
2022-02-05 20:17:17 +0900

    [feat] Update session option for store mysql database

--- backend/src/loaders/session.js
+++ backend/src/loaders/session.js
@@ -1,13 +1,18 @@
 const session = require('express-session')
+const DB_key = require('$/config').DB_key
+const MySQLStore = require('express-mysql-session')(session);
+const sessionStore = new MySQLStore(DB_key)
 const passport = require('passport')
 const LocalStrategy = require ('passport-local').Strategy
 const flash = require('connect-flash')
+const session_secret = require(__dirname+'/../config').session_secret;
 module.exports = ({app}) => {
-    const sessionKeySecret = require(__dirname+'/../config').session_key.secret;
+    
     app.use(session({
-        secret: sessionKeySecret,
+        secret: session_secret,
         resave: false,
-        saveUninitialized: true
+        saveUninitialized: true,
+        store: sessionStore
     }))
     app.use(passport.initialize())
     app.use(passport.session())
