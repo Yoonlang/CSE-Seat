15e71c1698c8cceab0a36888cca39508e5ef077c
yoonlang <cdt9473@gmail.com>
2022-02-05 23:39:37 +0900

    [feat] Add error page

---
+++ frontend/pages/_error.jsx
@@ -0,0 +1,18 @@
+const Error = () => {
+    return(
+        <>
+            <div>
+                잘못된 페이지입니다.
+            </div>
+            <style jsx>{`
+                div{
+                    width: 100%;
+                    height: 100%;
+                    background: #ddd;
+                }
+            `}</style>
+        </>
+    );
+}
+
+export default Error;
