1dbacf642f71b872a26f12bf3bf5cf05a938e082
yoonlang <cdt9473@gmail.com>
2022-02-07 00:20:57 +0900

    feat: Implement check room in Apply page

--- frontend/pages/apply.jsx
+++ frontend/pages/apply.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { PageDiv, StyledResDiv } from "../components/atoms/Div";
 import HeadTitle from "../components/others/headTitle"
@@ -7,10 +7,14 @@ const Apply = () => {
     const ApplyDiv = styled(StyledResDiv)`
         display: flex;
         flex-direction: column;
+        align-items: center;
         max-width: 523px;
         background: #fff;
         @media(min-width: 768px){
-            box-shadow: 0 -5px 10px 3px #ddd;
+            border: solid;
+            border-width: 0 1px;
+            border-color: #ddd;
+            box-shadow: 0 -5px 6px 2px #ddd;
         }
     `;
 
@@ -24,43 +28,157 @@ const Apply = () => {
         setIsRoomHope(tempRoomHope);
     }
 
-    useEffect(() => {
-        console.log(isRoomHope);
-    }, [isRoomHope])
-
     return (
         <PageDiv dis="flex" jus="center">
             <HeadTitle title="apply"/>
             <ApplyDiv>
-                <div>자리 신청</div>
+                <div className="title">자리 신청</div>
+                <div className="bar" />
+                <div className="room">
+                    <span>
+                        원하는 호실<br/>
+                        <span>(중복 가능)</span><br/><br/>
+                        <span>체크 안할 시 임의 배정</span>
+                    </span>
+                    <div className="roomBtn0" onClick={() => clickRoom(0)}>101호</div>
+                    <div className="roomBtn1" onClick={() => clickRoom(1)}>104호</div>
+                    <div className="roomBtn2" onClick={() => clickRoom(2)}>108호</div>
+                </div>
+                <div className="bar" />
+                <div className="seat">
+                    <span>
+                        원하는 자리<br/><br/>
+                        <span>미입력 시 임의 배정</span>
+                    </span>
+                    <div className="roomBtn" onClick={() => clickRoom(0)}>101호</div>
+                    <div className="roomBtn" onClick={() => clickRoom(1)}>104호</div>
+                </div>
+                <div className="bar" />
                 <div className="room">
                     <span>원하는 호실</span>
-                    <button onClick={() => clickRoom(0)}>101호</button>
-                    <button onClick={() => clickRoom(1)}>104호</button>
-                    <button onClick={() => clickRoom(2)}>108호</button>
+                    <div className="roomBtn" onClick={() => clickRoom(0)}>101호</div>
+                    <div className="roomBtn" onClick={() => clickRoom(1)}>104호</div>
+                    <div className="roomBtn" onClick={() => clickRoom(2)}>108호</div>
                 </div>
-                <div></div>
-                <div></div>
+                <button>신청하기</button>
             </ApplyDiv>
             <style jsx>{`
+                .title{
+                    padding: 50px 0 0 10%;
+                    width: 100%;
+                    height: 70px;
+                    font-size: 24px;
+                }
+                .bar{
+                    width: 85%;
+                    height: 1px;
+                    border:solid;
+                    border-width: 1px 0 0 0;
+                    border-color: #ddd;
+                    margin: 30px 0;
+                }
+                div > span{
+                    padding-left: 10px;
+                    width: 100%;
+                    font-size: 18px;
+                }
+                div > span > span{
+                    font-size: 14px;
+                }
                 .room{
                     display: grid;
                     grid-template-rows: 1fr 1fr 1fr;
                     grid-template-columns: 200px 1fr;
-                    width: 100%;
-                    height: 140px;
-                    background: #ddd;
-
+                    justify-items: center;
+                    align-items: center;
+                    width: 80%;
+                    height: 120px;
+                    row-gap: 6px;   
                 }
-                .room span {
+                .room > span {
                     grid-column: 1/1;
                     grid-row: 1/4;
                 }
-                .room button{
-                    width: 160px;
+                .room > div{
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 140px;
                     height: 35px;
+                    cursor: pointer;
+                    letter-spacing: 1px;
                 }
-                
+                .roomBtn0{
+                    border:solid;
+                    border-width: 1px;
+                    ${(isRoomHope[0] ? `
+                    background: #5C9EFF;
+                    border-color: #5C9EFF;
+                    color: #fff;
+                    ` : `
+                    background: #fff;
+                    border-color: #ddd;
+                    color: #000;
+                    `)}
+                }
+                .roomBtn1{
+                    border:solid;
+                    border-width: 1px;
+                    ${(isRoomHope[1] ? `
+                    background: #5C9EFF;
+                    border-color: #5C9EFF;
+                    color: #fff;
+                    ` : `
+                    background: #fff;
+                    border-color: #ddd;
+                    color: #000;
+                    `)}
+                }
+                .roomBtn2{
+                    border:solid;
+                    border-width: 1px;
+                    ${(isRoomHope[2] ? `
+                    background: #5C9EFF;
+                    border-color: #5C9EFF;
+                    color: #fff;
+                    ` : `
+                    background: #fff;
+                    border-color: #ddd;
+                    color: #000;
+                    `)}
+                }
+                .seat{
+                    display: grid;
+                    grid-template-rows: 1.2fr 0.8fr;
+                    grid-template-columns: 200px 1fr;
+                    justify-items: center;
+                    align-items: center;
+                    width: 80%;
+                    height: 100px;
+                    row-gap: 6px;
+                }
+                .seat > span{
+                    grid-column: 1/1;
+                    grid-row: 1/3;
+                }
+
+
+                button{
+                    width: 140px;
+                    height: 40px;
+                    margin: 80px 0 40px 0;
+                    outline: none;
+                    border: solid;
+                    border-width: 1px;
+                    border-color: #5C9EFF;
+                    border-radius: 0;
+                    background: #5C9EFF;
+                    color: #fff;
+                    font-size: 16px;
+                    font-weight: 600;
+                    letter-spacing: 4px;
+                    cursor: pointer;
+                }
             `}</style>
         </PageDiv>
     )
