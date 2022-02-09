a9c1a77642248427dbbacd88071bfbce4ec97c15
chanwooDev <pove2019@gmail.com>
2022-02-09 23:28:45 +0900

    test: Add console.log

--- backend/src/api/routes/user/login/index.js
+++ backend/src/api/routes/user/login/index.js
@@ -9,6 +9,7 @@ router.get('/', (req, res)=>{
 });
 
 router.post('/process', (req,res,next)=>{
+    console.log(req);
     passport.authenticate('local-login', (err,user,info) => {
         if(err) return next(err);
         if (!user) return res.status(401).json(info.message);
