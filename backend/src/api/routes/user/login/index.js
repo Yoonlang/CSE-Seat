6ecf4442c10616e14b6494b1f7f45910f0a23ddb
chanwooDev <pove2019@gmail.com>
2022-02-09 23:33:18 +0900

    fix: Remove console.log

--- backend/src/api/routes/user/login/index.js
+++ backend/src/api/routes/user/login/index.js
@@ -9,7 +9,6 @@ router.get('/', (req, res)=>{
 });
 
 router.post('/process', (req,res,next)=>{
-    console.log(req);
     passport.authenticate('local-login', (err,user,info) => {
         if(err) return next(err);
         if (!user) return res.status(401).json(info.message);
