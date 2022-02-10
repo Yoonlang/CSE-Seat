917a55e3510e08b5c629ad7d4262a4896cc1c303
chanwooDev <pove2019@gmail.com>
2022-02-11 03:18:26 +0900

    fix: Fix the name 'reservation_part' to 'part'

--- backend/src/models/reservation.js
+++ backend/src/models/reservation.js
@@ -3,7 +3,7 @@ let db = require('./mysql');
 module.exports = {
     findList : async (seatDTO) => new Promise( async (resolve, reject) => {
         let sql = "select * from reservation Where building_id = ? and seat_room = ? "
-        + "and date = ? order by reservation_part, seat_num ASC";
+        + "and date = ? order by part, seat_num ASC";
         let set  = [
             seatDTO.building_id,
             seatDTO.seat_room,
