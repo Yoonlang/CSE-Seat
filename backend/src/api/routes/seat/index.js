6c574cf19f326a38cf93fd4b9dc5a2849ffab719
chanwooDev <pove2019@gmail.com>
2022-02-11 03:19:45 +0900

    feat: Add console.log(err)

--- backend/src/api/routes/seat/index.js
+++ backend/src/api/routes/seat/index.js
@@ -16,6 +16,7 @@ router.post('/reserve',isLoggedIn,async (req, res, next) => {
 });
 
 router.use((err,req,res,next)=>{
+    console.log(err);
     res.status(500).json({result: false, message : err.message})
 })
 
