0561f6f70d7de54ed97a9a02ff5b7b7c5307227d
chanwooDev <pove2019@gmail.com>
2022-02-10 23:16:02 +0900

    feat: Add seat apply

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,9 +1,11 @@
 const {Router} = require('express')
 const user = require('./user')
 const main = require('./main')
+const seat = require('./seat')
 const path = require('path');
 const router = Router();
 
+router.use('/seat', seat);
 router.use('/user', user);
 router.use(main);
 
