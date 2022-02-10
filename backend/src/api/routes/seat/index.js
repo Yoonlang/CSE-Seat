5a2bb80dec9fa8b13e0c759ee95ce65744c7ae52
chanwooDev <pove2019@gmail.com>
2022-02-11 02:24:02 +0900

    feat: Rename func 'apply' to 'reserve' in routes

--- backend/src/api/routes/seat/index.js
+++ backend/src/api/routes/seat/index.js
@@ -7,18 +7,10 @@ router.get('/',async (req, res, next) => {
     res.status(200).json({result : true , data : 'hello'});
 });
 
-router.post('/apply',isLoggedIn,async (req, res, next) => {
+router.post('/reserve',isLoggedIn,async (req, res, next) => {
     let seatDTO = req.body;
     seatDTO.user_sid = req.user;
-    let result = await seatService.apply(seatDTO);
-    if(result instanceof Error) return next(result);
-    res.status(200).json({result: true});
-});
-
-router.post('/apply',isLoggedIn,async (req, res, next) => {
-    let seatDTO = req.body;
-    seatDTO.user_sid = req.user;
-    let result = await seatService.apply(seatDTO);
+    let result = await seatService.reserve(seatDTO);
     if(result instanceof Error) return next(result);
     res.status(200).json({result: true});
 });
