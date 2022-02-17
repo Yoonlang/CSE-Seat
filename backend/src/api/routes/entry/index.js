405d5157f04daa7831603df1119e37382b4f0463
chanwooDev <pove2019@gmail.com>
2022-02-17 19:36:34 +0900

    feat: Make entry routes basic frame

---
+++ backend/src/api/routes/entry/index.js
@@ -0,0 +1,24 @@
+const {Router} = require('express');
+const seatService = require('$/services/seat');
+const router = Router();
+const {isLoggedIn} = require('$/middlewares/auth');
+
+router.get('/',async (req, res, next) => {
+    res.status(200).json({result : true , data : 'hello'});
+});
+
+router.post('/check',isLoggedIn,async (req, res, next) => {
+    // let seatDTO = req.body;
+    // seatDTO.user_sid = req.user;
+    // let result = await seatService.reserve(seatDTO);
+    // if(result instanceof Error) return next(result);
+    res.status(200).json({result: true});
+});
+
+
+router.use((err,req,res,next)=>{
+    console.log(err);
+    res.status(500).json({result: false, message : err.message})
+})
+
+module.exports = router;
