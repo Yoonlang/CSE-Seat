a7f98b14e7b3c58ef881889faf21fcd825a231e2
chanwooDev <pove2019@gmail.com>
2022-02-19 17:52:11 +0900

    faet: Add login check

--- backend/src/api/routes/user/login/index.js
+++ backend/src/api/routes/user/login/index.js
@@ -14,9 +14,16 @@ router.post('/process', (req,res,next)=>{
         if (!user) return res.status(401).json({result:false ,message: info.message});
         req.logIn(user, function(err) {
             if (err) { return next(err); }
-            return res.status(200).json({result:true, sid: user.sid});
+            return res.status(200).json({result:true, sid: user});
         });
     })(req, res, next);
+});
+
+router.get('/check', (req,res,next)=>{
+    let user = req.user
+    if (!user) return res.status(401).json({result:false ,message: "로그인 되어 있지 않습니다."});
+    console.log(req.user);
+    return res.status(200).json({result:true, sid: user});
 });
 
 router.use((err,req,res,next)=>{
