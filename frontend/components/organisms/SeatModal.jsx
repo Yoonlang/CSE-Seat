071c9a713bd65a808a6f1dc1aeb64bd97e1301bb
yoonlang <cdt9473@gmail.com>
2022-03-05 19:18:07 +0900

    test: Test check-in, check-out post

--- frontend/components/organisms/SeatModal.jsx
+++ frontend/components/organisms/SeatModal.jsx
@@ -125,6 +125,34 @@ const SeatModal = () => {
         if (isReadyToRequest[0] | isReadyToRequest[1]) submitReq();
     }
 
+    const test = async (isCheckIn) => {
+        const leftURL = isCheckIn ? "/entry/check-in" : "/entry/check-out";
+        try {
+            const res = await fetch(process.env.NEXT_PUBLIC_API_URL + leftURL, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    building_id: "414",
+                    seat_room: roomNumber.toString(),
+                    seat_num: seatNumber.toString(),
+                    isToday: isToday,
+                    part: isMySeat,
+                })
+            })
+            const data = await res.json();
+            console.log(data);
+        }
+        catch (excep) {
+
+        }
+        finally {
+
+        }
+    }
+
     useEffect(() => {
         modalOutside.current.style.display = isModalOpen ? "flex" : "none";
         setOneColor(seatColor[one]);
@@ -166,10 +194,10 @@ const SeatModal = () => {
                         <>
                             <div className="check">
                                 <div>
-                                    <button>입실</button><span>22.01.19<span className="space" />06 : 24</span>
+                                    <button onClick={() => test(true)}>입실</button><span>22.01.19<span className="space" />06 : 24</span>
                                 </div>
                                 <div>
-                                    <button>퇴실</button><span>22.01.19<span className="space" />06 : 24</span>
+                                    <button onClick={() => test(false)}>퇴실</button><span>22.01.19<span className="space" />06 : 24</span>
                                 </div>
                             </div>
                             <button className="submit" onClick={clickBtn}>자리 수정</button>
