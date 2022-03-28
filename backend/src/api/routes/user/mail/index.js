6b17a152180f2e346e7bd41dc590f0522229c4df
chanwooDev <pove2019@gmail.com>
2022-03-28 21:08:06 +0900

    feat: Add mail servcie for join process

---
+++ backend/src/api/routes/user/mail/index.js
@@ -0,0 +1,20 @@
+const {Router} = require('express');
+const router = Router();
+const flash = require('connect-flash');
+const userService = require('$/services/user');
+
+router.get('/', (req, res)=>{
+    res.send({result : 'hi', sid :  req.user, address: appDir});
+});
+
+router.post('/', async(req, res) => {
+    result = await userService.mail(req.body.mail);
+    if(result instanceof Error) return next(result);
+    res.status(200).json({result : true, num : result});
+});
+
+router.use((err,req,res,next)=>{
+    res.status(400).json({result: false, message : err.message})
+})
+
+module.exports = router;
