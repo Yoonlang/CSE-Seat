003b6989e917371cea8a4e197c440d7a199884ee
yoonlang <cdt9473@gmail.com>
2022-04-02 21:22:51 +0900

    feat: Implement all of error handling in fetch

--- frontend/components/organisms/TodayInfo.jsx
+++ frontend/components/organisms/TodayInfo.jsx
@@ -40,10 +40,12 @@ const TodayInfo = () => {
                                 })
                             })
                             const data = await res.json();
+                            if (res.status === 400) throw "잠시 후 다시 시도해주세요";
                             if (data.result === true) setRefreshData(!refreshData);
-                            else throw ("Error!");
+                            else alert(data.message);
                         } catch (e) {
-                            console.log("Error: ", e);
+                            alert(e);
+                            router.replace(router.asPath);
                         }
                     }
                 })
