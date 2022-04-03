b98aabab21517885e7a0c3e15e876d071ca6b143
chanwooDev <pove2019@gmail.com>
2022-04-03 14:14:36 +0900

    feat: Add logger

--- backend/src/loaders/express.js
+++ backend/src/loaders/express.js
@@ -2,7 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const routes = require(path.join(__dirname , '/../api/routes'))
-const cors = require('cors')
+const cors = require('cors');
+const logger = require('$/middlewares/winston');
+const morgan = require('morgan');
+const combined = ':remote-addr :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"' 
+// 기존 combined 포멧에서 timestamp만 제거
+const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : combined; // NOTE: morgan 출력 형태 server.env에서 NODE_ENV 설정 production : 배포 dev : 개발
+console.log(morganFormat);
 
 module.exports = ({app}) => {
     app.use(express.static('public'));
@@ -12,6 +18,7 @@ module.exports = ({app}) => {
         origin: true,
         credentials: true,
     }));
+    app.use(morgan(morganFormat, {stream : logger.stream}));
     app.use('/api', routes);
     return app;
 }
