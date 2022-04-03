e82532b21b425f39a4e244c516d60bacc3473afa
yoonlang <cdt9473@gmail.com>
2022-04-03 11:09:24 +0900

    docs: Add comment in geolocation function

--- frontend/components/others/checkPos.js
+++ frontend/components/others/checkPos.js
@@ -19,12 +19,11 @@ export const isInLocation = async () => {
             const position = await getPosition(options);
             const {latitude, longitude} = position.coords;
             const dist = Math.pow(Math.pow(latitude - it4Pos.latitude, 2) + Math.pow(longitude - it4Pos.longitude, 2), 0.5);
-            isCheck = dist < 0.101 ? true : false;
+            isCheck = dist < 0.101 ? true : false; // 일단 0.101 원래는 0.001
         } catch (e) {
             if(e.code === 1) alert("입실을 위해 위치 엑세스를 허용하세요.");
         }
     }
     if(isCheck === false) alert("입실을 위해 건물 주변에 있어야 합니다.");
     return isCheck;
-    // return true;
 }
