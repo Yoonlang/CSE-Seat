6f4936f02ece587591b134909fade6c8718c7d6c
yoonlang <cdt9473@gmail.com>
2022-04-03 13:42:00 +0900

    design: Update head by url data

--- frontend/components/others/headTitle.jsx
+++ frontend/components/others/headTitle.jsx
@@ -5,6 +5,10 @@ const HeadTitle = ({ title }) => {
     return (
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta property="og:title" content="CSE Seat" />
+            <meta property="og:description" content="컴퓨터학부 자리 신청 서비스" />
+            <meta property="og:image" content="https://github.com/CSE-seat/CSE-Seat/blob/main/frontend/public/images/cse.png?raw=true" />
+            <link rel="shortcut icon" type="image/x-icon" href="images/cse.png" />
             <title>{title} | CSE Seat</title>
         </Head>
     );
