f01dc796b993f82854e37875df5b78b9197b53fe
yoonlang <cdt9473@gmail.com>
2022-05-30 15:36:03 +0900

    docs: Update notification

--- frontend/components/others/manageNotification.js
+++ frontend/components/others/manageNotification.js
@@ -1,6 +1,8 @@
-export const alertMessageInMobile = `이용불가 시간 안내\n104호 4/22 09:00 ~ 13:00`;
+//이용불가 시간 안내\n104호 4/22 09:00 ~ 13:00
+export const alertMessageInMobile = ``;
 export const AlertMessageInPC = () => {
+    //이용불가 시간 안내<br />104호 4/22 09:00 ~ 13:00
     return <>
-    이용불가 시간 안내<br />104호 4/22 09:00 ~ 13:00
+    
     </>
 }
